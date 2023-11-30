f231f336589dd4a7fe1c387942ecbc1598e12281
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-12-01 00:45:20 +0200

    commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     a = 0
     for i in range(len(sys.argv) - 1):
-        a = int(sys.argv[i + 1])
+        a = a + int(sys.argv[i + 1])
     print("{}".format(a))
