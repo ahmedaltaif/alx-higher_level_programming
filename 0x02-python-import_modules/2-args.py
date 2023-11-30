b04342a57f03beeda6b4aa7588361c6d5016835e
ahmedaltaif <ahmedaltaif79@gmail.com>
2023-12-01 00:23:25 +0200

    commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ar = len(sys.argv) - 1
+    if ar == 0:
+        print("0 arguments.")
+    elif ar == 1:
+        print("{} argument:".format(ar))
+    else:
+        print("{} arguments:".format(ar))
+    for i in range(ar):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
