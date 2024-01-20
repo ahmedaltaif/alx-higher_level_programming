12cdfabe155b17aa51219b1bc27b9b7d4903c215
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-01-21 01:58:54 +0300

    commit

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        if len(my_list) > 0:
+            length = len(my_list) - 1
+        for i in my_list:
+            print(':d'.format(my_list[l]))
+            l -= 1
