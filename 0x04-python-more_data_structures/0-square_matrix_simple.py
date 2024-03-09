c86f45a6e6b2d109c352a0f0409caee9fbc4a904
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-09 17:30:55 +0300

    commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        maping = map(lambda x: x**2, row)
+        new_matrix.append(list(maping))
+
+return new_matrix
