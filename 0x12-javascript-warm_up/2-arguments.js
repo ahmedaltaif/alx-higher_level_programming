b6103f759a7f21b4e6c163d56d458d20d34b4355
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-12 04:41:19 +0300

    commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arg = process.argv;
 
-if (arg.length === 2) {
-    console.log("No argument");
-} else if (arg.length === 3) {
-    console.log("Argument found");
+const args = process.argv;
+if (args.length === 2) {
+  console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");
+  console.log('Arguments found');
 }
