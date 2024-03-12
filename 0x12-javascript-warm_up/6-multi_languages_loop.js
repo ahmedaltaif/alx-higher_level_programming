cfcd0eaccbd2d42ceef949d8ae1dd24914d731b3
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-13 01:48:42 +0300

    commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myary = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < myary.length; i++) {
-    console.log(myary[i]);
+	console.log(myary[i]);
 }
