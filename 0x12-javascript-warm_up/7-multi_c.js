d71a779972f56c922b13d0692191f0f0ea3836e0
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-13 02:22:53 +0300

    commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
-const num = parseInt(argv[2], 10);
+const num = parseInt(args[2], 10);
 if (isNaN(num)) {
     console.log("Missing number of occurrences");
 } else {
-    for (let i = 0; i <= num; i++) {
+    for (let i = 0; i <= num - 1; i++) {
         console.log("C is fun");
     }
 }
