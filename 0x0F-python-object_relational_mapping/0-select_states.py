43da7b59362f173e5d816ba1f9a398df458d50de
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-17 05:37:34 +0300

    commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,12 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
-    for i in range(num_rows):
-        print(cur.fetchone())
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,11 @@
 #!/usr/bin/python3
+import sys
 import MySQLdb
 
-
-def main():
-
-    # connect
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    # cursor
-    c = db.cursor()
-
-    # execute query
-    c.execute("SELECT * FROM states WHERE BINARY name = '{:s}'\
-    ORDER BY id ASC".format(argv[4]))
-
-    # fetch
-    rows = c.fetchall()
-
-    # print
-    for row in rows:
-        print(row)
-
-    # close
-    c.close()
-    db.close()
-
 if __name__ == "__main__":
-    from sys import argv
-    main()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
