56f67dbdca22c4a6e8c0444f486383fca9dc3259
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-17 07:10:52 +0300

    commit

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,8 +4,10 @@ with N (upper N) from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                    user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2], db=argv[3]
+        )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
@@ -14,4 +16,3 @@ if __name__ == "__main__":
             print(row)
     cur.close()
     db.close()
-
