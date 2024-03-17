26e75533f5ec3798094d35847d1ca4ddfa2edf5a
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-18 02:55:24 +0300

    commit

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -15,8 +15,9 @@ if __name__ == "__main__":
     from the database.
     """
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]))
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
     session = Session()
