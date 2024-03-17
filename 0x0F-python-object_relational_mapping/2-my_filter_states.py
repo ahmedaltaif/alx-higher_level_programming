b6f949e67cce347777ace09e0ed52920022529ff
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-17 06:51:15 +0300

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,16 +4,20 @@ all values in the states table of hbtn_0e_0_usa"""
 import sys
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name='{:s}'\
-    ORDER BY states.id".format(argv[4])
-    cur.execute(sql)
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
+
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cur.close()
-    db.close()
+        print(row)
