b25944b87b69298999e4b5c416e3d28fb34e01fe
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-17 06:53:47 +0300

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """a script that takes in an argument and displays
 all values in the states table of hbtn_0e_0_usa"""
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
