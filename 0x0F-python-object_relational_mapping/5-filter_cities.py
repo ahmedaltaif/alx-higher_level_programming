79c5292746cd26d9cf57ff50eef201c240adea49
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-18 01:15:40 +0300

    commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         user=argv[1], passwd=argv[2], db=argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT cities.name, cities.id FROM cities JOIN states \
-    on cities.state_id = states.id WHERE states.name LIKE %s \
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
     ORDER BY cities.id", (argv[4],))
     rows = cur.fetchall()
 
