05b9cb680528ef788e88afb6f1e962e9fb429d57
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-18 02:33:44 +0300

    commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -21,4 +21,7 @@ if __name__ == "__main__":
     session = Session()
 
     for instance in session.query(State).order_by(State.id):
-        print('{0}: {1}'.format(instance.id, instance.name))
+        if instance is None:
+            print('Nothing')
+        else:
+            print('{0}: {1}'.format(instance.id, instance.name))
