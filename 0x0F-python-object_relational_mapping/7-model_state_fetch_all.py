cf85ebe8154256fefa49838233fc6a61b4343fd1
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-18 02:26:10 +0300

    commit

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,14 +2,15 @@
 """
 "script that lists all State objects from the database hbtn_0e_6_usa
 """
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sys import argv
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
     Access to the database and get the states
+
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
