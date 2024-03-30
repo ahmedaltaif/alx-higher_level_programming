39af50adfec6f41490da990ff065ee0099a783b7
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-03-30 03:48:03 +0300

    Python - Network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request
 
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as respo :
+    with urllib.request.urlopen(request) as respo:
         conntent = respo.read()
         print("Body response:")
         print("\t- type: {}".format(type(conntent)))
