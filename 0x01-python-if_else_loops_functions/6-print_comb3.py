e8fd6cee95e36b269a557d34aead47646f3e3366
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-05-26 23:26:18 +0300

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,5 @@ for x in range(10):
         if x + y != 17:
             if x != y and x < y:
                 print("{}{},".format(x, y), end=" ")
-        
+
 print("{}".format("89", end=""))
-        
