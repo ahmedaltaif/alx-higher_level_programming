9cdbb8023e4b109c1ddc3abd6af80c08b350b18a
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-05-28 09:29:19 +0300

    commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range(101):
-    if i % 3 == 0:
+for i in range(1, 101):
+    if i % 5 == 0 and i % 3 == 0:
+        i = "FizzBuzz"
+    elif i % 3 == 0:
         i = "Fizz"
     elif i % 5 == 0:
         i = "Buzz"
-    elif i % 5 == 0 and i % 3 == 0:
-        i = "FizzBuzz"
     print("{}".format(i), end=" ")
