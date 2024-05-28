7c0286fed85c7ff67819a2604cac29365e03d2a4
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-05-28 18:28:06 +0300

    commit

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    newstr = ""
+    for i in str:
+        if str.index(i) == n:
+            continue
+        else:
+            newstr += i  
+    return newstr
+
+
+
+
