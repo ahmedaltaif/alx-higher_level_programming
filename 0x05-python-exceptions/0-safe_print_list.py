2a8d84b41c38b57414c416ab23c274a5d175f4e1
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-05-29 00:22:33 +0300

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,27 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-            while i < x : 
-                print(my_list[i], end="")
-                i += 1
+        while i < x:
+            print(my_list[i], end="")
+            i += 1
     except:
         pass
     print()
     return i
-    
-
-
-
-
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
-        
