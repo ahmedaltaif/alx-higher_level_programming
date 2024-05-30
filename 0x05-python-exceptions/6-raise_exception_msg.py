5d2b1dc29b9a902a24d46831f1424e49603d8f7e
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-05-31 01:11:10 +0300

    6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,9 +1,4 @@
+#!/usr/bin/python3
 def raise_exception_msg(message=""):
-    raise NameError(massage)
+    raise TypeError(message)
 
-
-
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
