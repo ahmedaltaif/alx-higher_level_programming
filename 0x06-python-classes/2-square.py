825efff1191aaf8db6ca5ce2e64d9d6eba43f59c
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-01 17:57:45 +0300

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""empty class Square with size"""
+
+
+class Square:
+    """with privat size attribute"""
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be an integer")
+        self.__size = size
+
