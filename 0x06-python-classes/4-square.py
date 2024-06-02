4b75978c2c0eb1617f5879970e63efbfc21cf6d3
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-02 23:28:13 +0300

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,14 +14,18 @@ class Square:
 
     @property
     def size(self):
+        """getter func"""
         return self.__size
+
     @size.setter
     def size(self, value):
+        """setter func"""
         if type(value) is not int:
             raise TypeError('size must be an integer')
         elif value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def area(self):
         """return the area"""
         return self.__size * self.__size
