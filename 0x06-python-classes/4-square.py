40c89158794084b21415b75820736883391c54c7
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-02 23:25:04 +0300

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""empty class Square with size"""
+
+
+class Square:
+    """with privat size attribute"""
+    def __init__(self, size=0):
+        """The __init__ method initializes the size value of the square."""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+    def area(self):
+        """return the area"""
+        return self.__size * self.__size
