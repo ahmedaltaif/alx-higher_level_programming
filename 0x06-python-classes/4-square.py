b68c9618049279a3270b029773f056f5bd11a040
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-02 23:50:21 +0300

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,11 +5,6 @@
 class Square:
     """with privat size attribute"""
     def __init__(self, size=0):
-        """The __init__ method initializes the size value of the square."""
-        if type(size) is not int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
         self.__size = size
 
     @property
