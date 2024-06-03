6c24498a98736b8c4952fa6904ee74222b7a91dd
ahmedaltaif <ahmedaltaif79@gmail.com>
2024-06-03 13:25:34 +0300

    __pycache__

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """add tow num"""
-    if a is not int or float:
+    if type(a) not in (int, float):
         raise TypeError('a must be an integer')
-    if b is not int or float:
+    if type(b) not in (int, float):
         raise TypeError('b must be an integer')
-    return int(a) + int(b)
+    return int(a + b)
 
-    
